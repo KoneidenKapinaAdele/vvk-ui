78fbde466dee5785ede8697457beb6504f8b5be6
Antti Kekki <antti.kekki@solita.fi>
2016-05-19 22:21:27 +0300

    Lisätty painike oman sijainnin näyttämiseen

--- js/vvk-map.js
+++ js/vvk-map.js
@@ -1,3 +1,5 @@
+var solitaOfficeHkiLocation = [60.176839, 24.938555];
+
 var vvkMap = function() {
   this.markerForPlaceIdMap = {};
   this.map = null;
@@ -10,18 +12,33 @@ var vvkMap = function() {
   };
 
   this.createMap = function() {
-    this.map = L.map('map', {maxZoom: 22}).setView([60.176839, 24.938555], 19);
-    this.map.on('click', function(e) { console.log([e.latlng.lat, e.latlng.lng]); });
+    this.map = L.map('map', {maxZoom: 22})
+        .setView(solitaOfficeHkiLocation, 19)
+        .addControl(new vvkGoToHomeControl())
+        .on('click', function(e) { console.log([e.latlng.lat, e.latlng.lng]); });
 
+    L.control.scale({metric: true, imperial: false}).addTo(this.map);
+
+    // Open Street map layer
     L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(this.map);
 
+    // Solita office image overlay image
     var imageUrl = 'img/solita_helsinki_map_grayscale_flipped.png';
     var imageBounds = [[60.177211, 24.939080], [60.176510, 24.938305]];
 
     L.imageOverlay(imageUrl, imageBounds).addTo(this.map);
+
+    // Geo location
+    L.control.locate({
+      setView: 'untilPan',
+      keepCurrentZoomLevel: true,
+      locateOptions: {
+        enableHighAccuracy: true
+      }
+    }).addTo(this.map);
   };
 
   this.startPlaceStatusUpdatePoller = function() {
@@ -51,5 +68,33 @@ var vvkMap = function() {
     }
   };
 
-
 };
+
+var vvkGoToHomeControl = L.Control.extend({
+ 
+  options: {
+    position: 'topleft'
+  },
+ 
+  onAdd: function (map) {
+    var container = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-go-to-home');
+
+    var link = L.DomUtil.create('a', 'leaflet-bar-part leaflet-bar-part-single', container);
+    link.href = '#';
+    link.title = 'Go to Solita Helsinki office';
+
+    var icon = L.DomUtil.create('span', 'fa fa-home', link);
+
+    L.DomEvent
+        .on(link, 'click', L.DomEvent.stopPropagation)
+        .on(link, 'click', L.DomEvent.preventDefault)
+        .on(link, 'click', this. _onClick, this)
+
+    return container;
+  },
+
+  _onClick: function() {
+      this._map.setView(solitaOfficeHkiLocation, 19);
+  }
+ 
+});
