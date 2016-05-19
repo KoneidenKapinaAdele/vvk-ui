1abd4a0a1d712a5be575a08c5043e344350266a1
Antti Kekki <antti.kekki@solita.fi>
2016-05-19 23:23:45 +0300

    Haetaan tilojen status VVK-backendiltä

--- js/vvk-placeService.js
+++ js/vvk-placeService.js
@@ -1,20 +1,18 @@
 var vvkPlaceService = function() {
 
   this.getPlacesCurrentStatus = function(readyCallback) {
-    readyCallback([
-      {place_id: 1, latitude: 60.17667308232766, longitude: 24.93886023759842, occupied: this.randomBoolean()},
-      {place_id: 2, latitude: 60.17667274884376, longitude: 24.938826039433486, occupied: this.randomBoolean()},
-      {place_id: 3, latitude: 60.17667274884376, longitude: 24.93879653513432, occupied: this.randomBoolean()},
-      {place_id: 4, latitude: 60.1766710814243, longitude: 24.93876770138741, occupied: this.randomBoolean()},
-      {place_id: 5, latitude: 60.17660104972962, longitude: 24.9387750774622, occupied: this.randomBoolean()},
-      {place_id: 6, latitude: 60.17660238366804, longitude: 24.938867613673217, occupied: this.randomBoolean()},
-      {place_id: 7, latitude: 60.177032242507515, longitude: 24.93876770138741, occupied: this.randomBoolean()},
-      {place_id: 8, latitude: 60.1770305751063, longitude: 24.938737526535995, occupied: this.randomBoolean()},
-      {place_id: 9, latitude: 60.17710093936467, longitude: 24.93872813880444, occupied: this.randomBoolean()}
-    ]);
+    var xhr = new XMLHttpRequest();
+	xhr.open('GET', encodeURI(vvk.backendUrl + '/v1/status/current'));
+	xhr.setRequestHeader('Content-Type', 'application/json');
+	xhr.onload = function() {
+	    if (xhr.status === 200) {
+	        readyCallback(JSON.parse(xhr.responseText));
+	    }
+	    else {
+	        console.log('Request failed.  Returned status of ' + xhr.status);
+	    }
+	};
+	xhr.send();
   };
 
-  this.randomBoolean = function() {
-    return Math.random() > 0.5;
-  };
 };
--- js/vvk.js
+++ js/vvk.js
@@ -1,4 +1,5 @@
 var vvk = {};
+vvk.backendUrl = 'https://secret-oasis-98680.herokuapp.com';
 vvk.solitaOfficeHkiLocation = [60.176839, 24.938555];
 vvk.map = new vvkMap();
 vvk.placeService = new vvkPlaceService();
